ad2302a258968bf36d394e559a1532c2f44bcc50
altanmk93 <altanmk93@zedat.fu-berlin.de>
2019-04-12 00:29:28 +0200

    implemented /newpassword and /delpassword commands

--- telebot/bot.py
+++ telebot/bot.py
@@ -16,21 +16,11 @@ class PasswordManagerBot:
         self.main_path = os.path.dirname(self.file)
         self.state = ''
         self.query_data = ''
-
-    def get_passwords(self):
-        try:
-            with open(self.main_path + '/passwords.json') as json_file:
-                data = json.load(json_file)
-            passwords = data['passwords']
-            self.password_list = passwords
-        except IOError as error:
-            print("{} {}".format("Couldn't open file", error))
-
-    def parse_password(self, query_data):
-        for i in range(len(self.password_list)):
-            for key, value in self.password_list[i].iteritems():
-                if query_data == key:
-                    return value
+        self.new_password = False
+        self.succeed = False
+        self.site_name = ''
+        self.username = ''
+        self.password = ''
 
     def on_chat_message(self, msg):
         content_type, chat_type, chat_id = telepot.glance(msg)
@@ -38,14 +28,33 @@ class PasswordManagerBot:
             self.bot.sendMessage(chat_id, 'Alright your password has been changed!')
             self.set_password((msg[content_type]))
             self.state = ''
+        elif self.state == 'NEWPASSWORD':
+            if self.exist(msg['text']) and not self.new_password:
+                self.bot.sendMessage(chat_id, 'This site already exist. Try another one.')
+            elif self.new_password:
+                self.username = msg['text']
+                self.bot.sendMessage(chat_id, 'And finally the password.')
+                self.new_password = False
+                self.succeed = True
+            elif not self.new_password and not self.succeed:
+                self.site_name = msg['text']
+                self.bot.sendMessage(chat_id, 'Now type in the username.')
+                self.new_password = True
+            elif self.succeed and not self.new_password:
+                self.password = msg['text']
+                self.add_password()
+                self.bot.sendMessage(chat_id, 'Cool! I have added your password!')
+                self.state = ''
+                self.succeed = False
         else:
             if msg['text'] == '/start':
-                start_text = "{} {} {} {} {} {}".format('Hello my name is ', self.bot.getMe()['first_name'],
-                                                        'and i am a bot to manage your passwords.'
-                                                        '\n\nYou can control me by sending these commands:\n\n',
-                                                        '/newpassword - save your password\n',
-                                                        '/getpassword - get your password\n',
-                                                        '/setpassword - set your password\n')
+                start_text = "{} {} {} {} {} {} {}".format('Hello my name is ', self.bot.getMe()['first_name'],
+                                                           'and i am a bot to manage your passwords.' 
+                                                           '\n\nYou can control me by sending these commands:\n\n',
+                                                           '/newpassword - save your password\n',
+                                                           '/getpassword - get your password\n',
+                                                           '/setpassword - set your password\n',
+                                                           '/delpassword - delete your password\n')
                 self.bot.sendMessage(chat_id, start_text)
             elif msg['text'] == '/getpassword':
                 self.get_passwords()
@@ -63,21 +72,77 @@ class PasswordManagerBot:
                     self.bot.sendMessage(chat_id, 'Which password do you want to change?', reply_markup=keyboard)
                 else:
                     self.bot.sendMessage(chat_id, 'Currently there are no passwords.')
+            elif msg['text'] == '/newpassword':
+                self.state = 'NEWPASSWORD'
+                self.bot.sendMessage(chat_id, 'First type in the name of the site')
+            elif msg['text'] == '/delpassword':
+                self.get_passwords()
+                keyboard = self.get_keyboard()
+                if keyboard is not None:
+                    self.state = 'DELPASSWORD'
+                    self.bot.sendMessage(chat_id, 'Which password do you want to delete?', reply_markup=keyboard)
+                else:
+                    self.bot.sendMessage(chat_id, 'Currently there are no passwords.')
             else:
                 self.bot.sendMessage(chat_id, "I couldn't understand this command. Type /start to see all commands.")
 
     def on_callback_query(self, msg):
-        print("{} {}".format('state:', self.state))
         query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
         password = self.parse_password(query_data)
         self.bot.answerCallbackQuery(query_id, text='Got it')
         if self.state == 'GETPASSWORD':
-            response = "{} {} {} {}".format("The username is", password['username'],
-                                            "and the password is", password['password'])
+            response = "{} {} {} {} {} {}".format('Your username for', query_data, 'is', password['username'],
+                                                  'and the password is', password['password'])
             self.bot.sendMessage(from_id, response)
+        elif self.state == 'DELPASSWORD':
+            self.query_data = query_data
+            new_dict = self.delete_dict()
+            self.write_to_file(new_dict)
+            self.state = ''
+            self.bot.sendMessage(from_id, "{} {} {}".format('Alright, the password for',
+                                                            self.query_data, 'will be deleted'))
         else:
-            self.bot.sendMessage(from_id, 'Type in the new password')
+            self.bot.sendMessage(from_id, 'Please type in the new password')
             self.query_data = str(query_data)
+
+    def get_passwords(self):
+        try:
+            with open(self.main_path + '/passwords.json') as json_file:
+                data = json.load(json_file)
+            passwords = data['passwords']
+            self.password_list = passwords
+        except IOError as error:
+            print("{} {}".format("Couldn't open file", error))
+
+    def parse_password(self, query_data):
+        for i in range(len(self.password_list)):
+            for key, value in self.password_list[i].iteritems():
+                if query_data == key:
+                    return value
+
+    def write_to_file(self, dictionary):
+        try:
+            with open(self.main_path + '/passwords.json', 'w') as json_file:
+                json.dump(dictionary, json_file)
+        except IOError as error:
+            print("{} {}".format("Couldn't write to file", error))
+
+    def exist(self, site):
+        exist = False
+        self.get_passwords()
+        for i in range(len(self.password_list)):
+            if site in self.password_list[i]:
+                exist = True
+        return exist
+
+    def add_password(self):
+        passwords = {}
+        site = {}
+        site.update({self.site_name: {"username": self.username, "password": self.password}})
+        self.get_passwords()
+        self.password_list.append(site)
+        passwords.update({"passwords": self.password_list})
+        self.write_to_file(passwords)
 
     def get_keyboard(self):
         keys = []
@@ -101,11 +166,18 @@ class PasswordManagerBot:
                 if self.query_data == key:
                     self.password_list[i][key]['password'] = new_password
         d.update({"passwords": self.password_list})
-        try:
-            with open(self.main_path + '/passwords.json', 'w') as json_file:
-                json.dump(d, json_file)
-        except IOError as error:
-            print("{} {}".format("Couldn't write to file", error))
+        self.write_to_file(d)
+
+    def delete_dict(self):
+        self.get_passwords()
+        d = {}
+        for i in range(len(self.password_list)):
+            for key, value in self.password_list[i].items():
+                if key == self.query_data:
+                    del self.password_list[i]
+                    break
+            d.update({"passwords": self.password_list})
+        return d
 
 
 if __name__ == '__main__':
